3ddfdb03b3e26603426a9256d39e8a690bc3a899
João Vitor Silva Teixeira <123509118+dgzdev@users.noreply.github.com>
2024-02-19 16:42:39 -0300

    add new animations

--- GamePlace/src/StarterPlayer/StarterCharacterScripts/Health.server.lua
+++ GamePlace/src/StarterPlayer/StarterCharacterScripts/Health.server.lua
@@ -1,20 +1,71 @@
+local Debris = game:GetService("Debris")
+local SoundService = game:GetService("SoundService")
 -- Gradually regenerates the Humanoid's Health over time.
 
-local REGEN_RATE = 10 / 100 -- Regenerate this fraction of MaxHealth per second.
-local REGEN_STEP = 1 -- Wait this long between each regeneration step.
+local REGEN_RATE = 4 / 100 -- Regenerate this fraction of MaxHealth per second.
+local REGEN_STEP = 10 -- Wait this long between each regeneration step.
 
 --------------------------------------------------------------------------------
 
 local Character = script.Parent
-local Humanoid = Character:WaitForChild("Humanoid")
+local Humanoid = Character:WaitForChild("Humanoid") :: Humanoid
+local Head = Character:WaitForChild("Head") :: BasePart
+local LastHealth = Humanoid.MaxHealth
 
 --------------------------------------------------------------------------------
 
-while true do
-	while Humanoid.Health < Humanoid.MaxHealth do
-		local dt = task.wait(REGEN_STEP)
-		local dh = dt * REGEN_RATE * Humanoid.MaxHealth
-		Humanoid.Health = math.min(Humanoid.Health + dh, Humanoid.MaxHealth)
+local MIN_DISTANCE = 0
+local MAX_DISTANCE = 30
+local COOLDOWN = 0.05
+local HEALCOOLDOWN = 3
+local LAST_TAKEN_DAMAGE = tick()
+local LAST_HEAL = tick()
+
+local HealSound = SoundService:WaitForChild("SFX"):WaitForChild("Heal")
+
+Humanoid.HealthChanged:Connect(function(health)
+	if LAST_TAKEN_DAMAGE + COOLDOWN > tick() then
+		return
 	end
-	Humanoid.HealthChanged:Wait()
-end
+
+	if health < LastHealth then
+		local SFX = SoundService:WaitForChild("Hit"):GetChildren()
+		local Sound = SFX[math.random(1, #SFX)]:Clone() :: Sound
+		Sound.Parent = Head
+
+		Sound.RollOffMinDistance = MIN_DISTANCE
+		Sound.RollOffMaxDistance = MAX_DISTANCE
+		Sound.RollOffMode = Enum.RollOffMode.Linear
+		Sound.Volume = Sound.Volume * math.random(0.9, 1.1)
+
+		Sound:Play()
+		Debris:AddItem(Sound, Sound.TimeLength + 0.1)
+		LAST_TAKEN_DAMAGE = tick()
+	elseif health > LastHealth then
+		if LAST_HEAL + HEALCOOLDOWN > tick() then
+			return
+		end
+		local heal = HealSound:Clone()
+		heal.Parent = Head
+
+		heal.RollOffMinDistance = MIN_DISTANCE
+		heal.RollOffMaxDistance = MAX_DISTANCE
+		heal.RollOffMode = Enum.RollOffMode.Linear
+
+		heal:Play()
+		Debris:AddItem(heal, heal.TimeLength + 0.2)
+		LAST_HEAL = tick()
+	end
+	LastHealth = health
+end)
+
+task.spawn(function()
+	while true do
+		while Humanoid.Health < Humanoid.MaxHealth do
+			local dt = task.wait(REGEN_STEP)
+			local dh = dt * REGEN_RATE * Humanoid.MaxHealth
+			Humanoid.Health = math.min(Humanoid.Health + dh, Humanoid.MaxHealth)
+		end
+		Humanoid.HealthChanged:Wait()
+	end
+end)
