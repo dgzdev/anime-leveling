52ac62f63c7e23b01fa0cd85863c6d4d0ae763a9
rodexiy <daniel.da.rocha0506@gmail.com>
2024-06-11 19:32:03 -0300

    GoldService

---
+++ GamePlace/Game/ServerStorage/Services/Data/GoldService.lua
@@ -0,0 +1,56 @@
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local Knit = require(game.ReplicatedStorage.Packages.Knit)
+
+local GoldService = Knit.CreateService {
+    Name = "GoldService",
+    Client = {},
+}
+
+local PlayerService
+
+function GoldService:HaveGold(Player: Player, Amount: number): boolean
+    local PlayerData = PlayerService:GetData(Player)
+    return PlayerData.Gold >= Amount 
+end
+
+function GoldService:RemoveGold(Player: Player, Amount: number): number | boolean
+    local PlayerData = PlayerService:GetData(Player)
+    if not GoldService:HaveGold(Player, Amount) then
+        return false
+    end
+
+    PlayerData.Gold -= Amount
+    return PlayerData.Gold
+end
+
+function GoldService:AddGold(Player: Player, Amount: number): number
+    local PlayerData = PlayerService:GetData(Player)
+    PlayerData.Gold += Amount
+    return PlayerData.Gold
+end
+
+function GoldService:SetGold(Player: Player, Amount: number)
+    local PlayerData = PlayerService:GetData(Player)
+    PlayerData.Gold = Amount
+end
+
+-- caso for possivel comprar, executa o callback
+function GoldService:Buy(Player: Player, Amount: number, callback)
+    if GoldService:HaveGold(Player, Amount) then
+        GoldService:RemoveGold(Player, Amount)
+        callback(true)
+    end
+    
+end
+
+
+
+function GoldService.KnitInit()
+    PlayerService = Knit.GetService("PlayerService")    
+end
+
+function GoldService.KnitStart()
+    
+end
+
+return GoldService
